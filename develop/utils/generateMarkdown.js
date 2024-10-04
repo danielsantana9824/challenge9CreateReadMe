8d4e6622fc37e564296a8f16445b3f6230ce98fb
danielsantana9824 <danielsantana9824@gmail.com>
2024-10-04 05:23:41 -0500

    template
    i modified the template, i change the static data to dynamic data

--- develop/utils/generateMarkdown.js
+++ develop/utils/generateMarkdown.js
@@ -41,20 +41,14 @@ function renderLicenseSection(license) { }
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
 
-  return `# 
-  ${renderLicenseBadge(data.license)} ;
+  return `
+  # Title: ${data.title}
 
-
-  # <Your-Project-Title>
+## Name of Project: ${data.nameProject}.
 
 ## Description
 
-Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
-
-- What was your motivation?
-- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-- What problem does it solve?
-- What did you learn?
+${data.description}
 
 ## Table of Contents (Optional)
 
@@ -67,17 +61,11 @@ If your README is long, add a table of contents to make it easy for users to fin
 
 ## Installation
 
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+${data.installation}
 
 ## Usage
 
-Provide instructions and examples for use. Include screenshots as needed.
-
-To add a screenshot, create an assets/images folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
-
-     md
-     ![alt text](assets/images/screenshot.png)
-    
+${data.usage}
 
 ## Credits
 
@@ -87,31 +75,29 @@ If you used any third-party assets that require attribution, list the creators w
 
 If you followed tutorials, include links to those here as well.
 
+## Command to run
+
+to run the system, you need to use: ${data.command}
+
 ## License
 
-The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+${data.license}
 
----
-
-🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-
-## Badges
-
-![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-
-Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-
-## Features
-
-If your project has a lot of features, list them here.
 
 ## How to Contribute
 
-If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+${data.contributing}
+
 
 ## Tests
 
-Go the extra mile and write tests for your application. Then provide examples on how to run them here.
+${data.test}
+
+## Contact
+
+This is my user name from gitHub: ${data.username}.
+
+This is my email: ${data.email}
 
 `;
 }
